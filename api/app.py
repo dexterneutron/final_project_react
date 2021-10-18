c70d6646bc4db1f81313b645169f14a6cdf13383
DESKTOP-2QIG43J\felix.lugo <felixdlugo@gmail.com>
2021-10-18 17:43:24 -0400

    Scheduled data retrieval

--- api/app.py
+++ api/app.py
@@ -1,14 +1,34 @@
 import sqlalchemy as db
 from flask import Flask, jsonify
 from flask_cors import CORS
+from flask_apscheduler import APScheduler
+from etl import run_etl
+
+class Config:
+    SCHEDULER_API_ENABLED = True
 
 app = Flask(__name__)
+app.config.from_object(Config())
+
 CORS(app)
+
+scheduler = APScheduler()
+scheduler.init_app(app)
+scheduler.start()
 
 @app.route("/api/articles", methods = ['GET'])
 def retrive_data():
     data = get_articles()
     return jsonify(data)
+
+@scheduler.task('interval', id='update_from_source', seconds=60, misfire_grace_time=900)
+def update_from_source():
+    try:
+        run_etl()
+        print('Data updated sucessfully')
+    except:
+        print('Error fetching data')
+
 
 def get_articles():
     engine = db.create_engine('sqlite:///articles.sqlite')
